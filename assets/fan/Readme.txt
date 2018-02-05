6eaf9231eb1ba6d871314370c41d04aeea058148
Amit <pardeshi.amitsingh@gmail.com>
2018-02-05 08:47:12 -0600

    added few models and updated textures

---
+++ assets/fan/Readme.txt
@@ -0,0 +1,21 @@
+Here is a animated fan made with Blender.
+
+Animated Yes
+Rigged Yes
+Lowpoly (game-ready) Yes
+Geometry Subdivision ready
+Polygons 490
+Vertices 676
+Textures Yes
+Materials Yes
+UV Mapping Yes
+Unwrapped UVs Mixed
+
+Demo Video: https://www.youtube.com/watch?v=5-ADSn4AmHw
+3d-Preview on Sketchfab : https://sketchfab.com/models/1757576875664d9ab891168a4c01b521
+
+Homepage: http://3dartdh.wordpress.com/
+Contact me: https://3dartdh.wordpress.com/contact/
+Sketchfab: https://sketchfab.com/dennish2010
+YouTube: https://www.youtube.com/user/DennisH2010
+
--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-document.write('<script type="text/javascript" src="libs/dat.gui.min.js"></script>');
 
 var sceneEl = document.querySelector('a-scene');
 
@@ -6,13 +5,63 @@ var camera = document.querySelector('a-camera');
 var entity = sceneEl.querySelector('a-entity');
   var wallLight ;
 
-document.querySelector('a-scene').addEventListener('loaded', function () {
-	var gui = new dat.GUI();
-    
-    wallLight = sceneEl.querySelector('#wallLight');
- //   var lightsphere = sceneEl.querySelector('#lightsphere');
-    wallLight.setAttribute('intensity', 2);
+document.querySelector('#cubeshelf').addEventListener('click', function () {
+	var mobileshelf = document.querySelector('#mobileshelf');
+	var cubeshelf = document.querySelector('#cubeshelf');
 
-  //  lightsphere.position.y = 
-   // gui.add(wallLight, 'intensity', 0, 10);
-})
+  	if(mobileshelf.getAttribute('position').x == 45  && 
+  		mobileshelf.getAttribute('position').y == 0 && 
+  		mobileshelf.getAttribute('position').z == 24  ) {
+  		mobileshelf.setAttribute('position', '45 0 20');
+  		cubeshelf.setAttribute('position', '45 0 20');
+  	} else {
+  		mobileshelf.setAttribute('position', '45 0 24');
+  		cubeshelf.setAttribute('position', '45 0 24');
+  	}
+});
+
+//left light turn on off
+document.querySelector('#cubellight').addEventListener('click', function () {
+	var llight = document.querySelector('#llight');
+
+  	if(llight.getAttribute('intensity') == 0 ) {
+  		llight.setAttribute('intensity', '4');
+  	} else {
+  		llight.setAttribute('intensity', '0');
+  		
+  	}
+});
+
+//right light turn on off
+document.querySelector('#cuberlight').addEventListener('click', function () {
+	var rlight = document.querySelector('#rlight');
+
+  	if(rlight.getAttribute('intensity') == 0 ) {
+  		rlight.setAttribute('intensity', '4');
+  	} else {
+  		rlight.setAttribute('intensity', '0');
+  		
+  	}
+});
+
+//Turn on off fire
+document.querySelector('#cubefireplace').addEventListener('click', function () {
+	var fire = document.querySelector('#fire');
+	var firelight = document.querySelector('#firelight');
+
+  	if(fire.getAttribute('visible') == true ) {
+  		fire.setAttribute('visible', 'false');
+  		firelight.setAttribute('visible', 'false');
+  		firelight.setAttribute('intensity', '0');
+  	} else {
+  		var animation = document.createElement("a-animation");
+  		animation.setAttribute("attribute","position");
+        animation.setAttribute("to","55 2 -14");
+            animation.setAttribute("dur","450");
+            animation.setAttribute("repeat","indefinite");
+        //    firelight(animation);
+  		fire.setAttribute('visible', 'true');
+  		firelight.setAttribute('visible', 'true');
+  		firelight.setAttribute('intensity', '0.1');
+  	}
+});
